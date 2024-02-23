f4cad22217bae2300f4ba1712dd05aae21f11317
Thang <thangdq2003038@ivy.edu.vn>
2024-02-23 21:25:10 +0700

    Buoi1

---
+++ Buoi1/console.py
@@ -0,0 +1,13 @@
+#print
+print('a')
+# variable
+name = 'a'
+print(type(name))
+# name * 3
+print(name*3)
+# age 
+age = 10
+print(str(age) + name)
+# input
+userName = input('Type your name: ')
+print('You are ' + userName)
---
+++ Buoi1/turtle_draw.py
@@ -0,0 +1,51 @@
+from turtle import *
+
+#direction
+
+#pen
+pensize(5)
+color('pink')
+shape(name='turtle')
+#square
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+#pentagon   
+up()
+setposition(-300, 0)
+down()
+
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)  
+# circle
+up()
+setposition(100, 40)
+down()
+circle(radius=60    )
+# star 
+up()
+setposition(100, 100)
+down()
+left(180-36)
+forward(100)
+left(180-36)
+forward(100)
+left(180-36)
+forward(100)
+left(180-36)
+forward(100)
+left(180-36)
+forward(100)
+mainloop()
